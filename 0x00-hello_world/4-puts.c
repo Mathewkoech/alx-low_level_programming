a55ce005d60ee19b1e16abb812b9671249202fee
mathewkoech <mathewkoech55@gmail.com>
2023-10-05 10:46:04 -0700

    a script that prints out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
-include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ *Return: Always 0 (Success)
+ */
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
