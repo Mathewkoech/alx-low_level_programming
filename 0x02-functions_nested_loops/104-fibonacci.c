2ba5a3a91acf026d178777d4e735be4115497bcb
mathewkoech <mathewkoech55@gmail.com>
2023-10-11 10:55:04 -0700

    rogram that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Calculate and print the first 98 Fibonacci numbers
+ * main - finds and prints the first 98 Fibonacci numbers
  *
- * This function calculates and prints the first 98 Fibonacci numbers,
- * starting with 1 and 2, separated by commas.
- *
- * Return: Always Success (0).
+ * Return: Always 0
  */
 int main(void)
 {
 	int n = 98;
-	unsigned long a = 1, b = 2;
+	unsigned int a = 1, b = 2, next;
 
-	printf("%lu, %lu", a, b);
+	printf("%u, %u", a, b);
 
 	for (int i = 2; i < n; i++)
 	{
-		unsigned long next = a + b;
-
-		printf(", %lu", next);
-
+		next = a + b;
+		printf(", %u", next);
 		a = b;
 		b = next;
-
 	}
 
 	printf("\n");
