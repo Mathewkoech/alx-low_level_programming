57a21ec15a7421b699bd4e1feeab4f6ef93dd00e
mathewkoech <mathewkoech55@gmail.com>
2023-10-19 12:30:30 -0700

    prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,25 +1,51 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_buffer - Prints a buffer.
- * @b: The buffer to print.
- * @size: The number of bytes to print.
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
-int ,o, i, j;
-o = 0
+	int o, j, i;
 
-if (size <= 0)
-{
-printf('\n');
-return;
+	o = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
 }
 
-while (o < size)
-{
-j = size - o < 10 ? size - o : 10;
-printf("%08x: ", o)   
-   
-for (i = 0; i < size; i += 10)
-    
