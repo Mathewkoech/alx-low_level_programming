ef053430dddc54c3cf0ae77ab44e8aa7bf3c0a7a
mathewkoech <mathewkoech55@gmail.com>
2023-11-06 05:37:34 -0800

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d:pointer to struct dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+		if (d->owner != NULL)
+		{
+			free(d->owner);
+		}
+	}
+	free(d);
+}
