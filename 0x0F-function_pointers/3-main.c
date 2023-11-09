8ef7cb5b2817848bcfb000a029e91048868026a8
mathewkoech <mathewkoech55@gmail.com>
2023-11-09 06:29:48 -0800

    performs simple operation

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,13 +11,10 @@
 *exit with status 98, 99, or 100.
 */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int num1;
-	char *operator;
-	int num2;
-	int result;
-	int (*operation)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -26,19 +23,20 @@ int main(int argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	operator = argv[2];
+	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	operation = get_op_func(operator);
-
-	if (!operation)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	result = operation(num1, num2);
-	printf("%d\n", result);
-
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
