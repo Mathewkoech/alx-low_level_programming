b34a54067e27aed83290882710fea9f1c4ab34f1
mathewkoech <mathewkoech55@gmail.com>
2023-11-09 04:00:18 -0800

    function that perform simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -25,13 +25,10 @@ int (*get_op_func(char *s))(int, int)
 		};
 		int i = 0;
 
-		while (ops[i].op != NULL)
+		while (ops[i].op != NULL && *(ops[i].op) != *s)
 		{
-			if (strcmp(s, ops[i].op) == 0)
-			{
-				return (ops[i].f);
-			}
 			i++;
 		}
+
 		return (NULL);
 }
