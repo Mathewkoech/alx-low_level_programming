79fd2c9654fd2286a155bce1289b65c8ac11c165
mathewkoech <mathewkoech55@gmail.com>
2023-11-16 11:57:36 -0800

    64-bit program in assembly that prints

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+section .data
+    hello db 'Hello, Holberton', 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    sub rsp, 8
+    mov rdi, hello
+    call printf
+    add rsp, 8
+
+    mov eax, 0
+    ret
