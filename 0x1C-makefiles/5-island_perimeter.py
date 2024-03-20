3d84c6508fd9a10cb722d4f821ceeeb38d7c3ba3
mathewkoech <mathewkoech55@gmail.com>
2024-03-20 10:18:31 -0700

    pyton script to calculate perimeter of the island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""module calculates island perimeter"""
+
+"""
+This module calculates the island perimeter
+"""
 
 
 def island_perimeter(grid):
@@ -7,27 +10,30 @@ def island_perimeter(grid):
     Calculate the island perimeter
 
     Args:
-        grid (any): a list of list of integers 0 that has
-        0 as water zone and 1 as land zone
+    grid (list of list of int):
+    Represents the island grid where 1 represents land and 0 represents water
 
     Returns:
-        the perimeter of the grid
+        int: The perimeter of the island
     """
+    perimeter = 0
+
     if not grid:
-        return 0
+        return perimeter
+
     height = len(grid)
     width = len(grid[0])
-    if height > 100 or width > 100:
-        return "width or height should not exceed 100"
 
-    perimeter = []
-    for i in range(0, height):
-        for j in range(0, width):
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                top = grid[i-1][j]
-                below = grid[i+1][j]
-                left = grid[i][j-1]
-                right = grid[i][j+1]
-                cell_neighbours = [top, below, left, right]
-                perimeter.extend(cell_neighbours)
-    return perimeter.count(0)
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1  # Count top edge
+                if i == height - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1  # Count bottom edge
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1  # Count left edge
+                if j == width - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1  # Count right edge
+
+    return perimeter
